6e6ab0c3da46b2d6adf3a971d2b19dbdc4d6f637
Maheswaridillibabu <magesdillibabu27112002@gmail.com>
2025-06-08 07:24:01 +0530

    Initial commit

--- job-tracker-backend/server.js
+++ job-tracker-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
